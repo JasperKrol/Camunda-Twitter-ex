be3f00b5f7017ec752cee4652f051459f2264db7
Jasper Krol <jasper.krol@incentro.com>
2023-04-18 10:39:38 +0200

    5

--- src/main/java/com/camunda/training/service/TwitterService.java
+++ src/main/java/com/camunda/training/service/TwitterService.java
@@ -1,6 +1,5 @@
 package com.camunda.training.service;
 
-import com.camunda.training.delegate.CreateTweetDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -8,9 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TwitterService {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(CreateTweetDelegate.class.getName());
+    private final Logger LOGGER = LoggerFactory.getLogger(TwitterService.class.getName());
 
     public void updateStatus(String content) {
-        LOGGER.info("Tweet: " + content);
+        LOGGER.info(String.format("Tweet: " + content));
     }
 }
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,21 +1,26 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
 @ExtendWith(ProcessEngineCoverageExtension.class)
 class ProcessJUnitTest {
 
-    // TODO: 17-04-2023 fails because the task is still usertask-> can be set as manualtask so it will skipp
+    // provide a field where the process engine gets injected
+    ProcessEngine processEngine;
 
     @Test
     @Deployment(resources = "ex3-scripttask.bpmn")
@@ -28,10 +33,57 @@ class ProcessJUnitTest {
 
 
         // Start process with Java API and variables
+
         ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("ex3-scriptTask", variables);
 
         // Make assertions on the process instance
         assertThat(processInstance).isEnded();
     }
 
+//    @Test
+//    @Deployment(resources = "ex5-UserTask.bpmn")
+//    void happyPathEx5() {
+//
+//        // Create a HashMap to put in variables for the process instance
+//        RuntimeService runtimeService = extension.getRuntimeService();
+//
+//        // Start process with Java API and variables
+//        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("ex5-UserTask.bpmn", variables);
+//
+//
+//        List<Task> taskList = taskService()
+//                .createTaskQuery()
+//                .taskCandidateGroup("management")
+//                .processInstanceId(processInstance.getId())
+//                .list();
+//        // Make assertions on the process instance
+//
+//
+//        assertThat(taskList).isNotNull();
+//        assertThat(taskList).hasSize(1);
+//
+//        Task task = taskList.get(0);
+//
+//
+//        Map<String, Object> approvedMap = new HashMap<String, Object>();
+//        approvedMap.put("approved", true);
+//        taskService().complete(task.getId(), approvedMap);
+//
+//        assertThat(processInstance).isEnded();
+//
+//    }
+
+//    @Test
+//    @Deployment
+//    public void extensionUsageExample() {
+//        RuntimeService runtimeService = processEngine.getRuntimeService();
+//        runtimeService.startProcessInstanceByKey("extensionUsage");
+//
+//        TaskService taskService = processEngine.getTaskService();
+//        Task task = taskService.createTaskQuery().singleResult();
+//        assertThat(task.getName()).isEqualTo("My Task");
+//
+//        taskService.complete(task.getId());
+//        assertThat(runtimeService.createProcessInstanceQuery().count()).isEqualTo(0);
+//    }
 }
